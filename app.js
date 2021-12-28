cc6edb928d2c3166059739696b4cde7c8819275f
Syed12Gen <96316783+Syed12Gen@users.noreply.github.com>
2021-12-28 14:31:22 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const button = document.querySelector('button')
-const h1 = document.querySelector('h1')
-button.addEventListener('click', function(){
+const makeRandColor = () => {
 const r = Math.floor(Math.random()*255);
 const g = Math.floor(Math.random()*255);
 const b = Math.floor(Math.random()*255);
-const newColor = `rgb(${r}, ${g}, ${b})`;
-document.body.style.backgroundColor = newColor;
-h1.innerText = newColor;
+return `rgb(${r}, ${g}, ${b})`;
+}
 
 
-})
+const buttons = document.querySelectorAll('button')
+for (let button of buttons){
+    button.addEventListener('click', function(){
+       button.style.backgroundColor = makeRandColor();
+       console.log("Clicked Color Generated")
+    })
 
-
+    
+}
